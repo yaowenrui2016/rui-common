db48e5276c612d6a99e1708e219f0913c34bf997
yaowr <yao_20101991@163.com>
2019-07-05 09:07:30 +0800

    基础包 第3次 提交

---
+++ common-base/src/main/java/indi/rui/common/base/service/AbstractService.java
@@ -0,0 +1,12 @@
+package indi.rui.common.base.service;
+
+public class AbstractService<M, E, V> {
+
+    protected void entityToVo(E e, V v) {
+
+    }
+
+    protected void voToEntity(V v, E e) {
+
+    }
+}
