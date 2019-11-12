2f434cebb59f5db4f2ef7618a1b1be05e5506a0b
yaowr <yao_20101991@163.com>
2019-11-12 17:54:31 +0800

    附件上传

--- common-web/src/main/java/indi/rui/common/web/dao/IMapper.java
+++ common-web/src/main/java/indi/rui/common/web/dao/IMapper.java
@@ -3,11 +3,12 @@ package indi.rui.common.web.dao;
 import indi.rui.common.base.dto.QueryRequest;
 import indi.rui.common.base.field.IFieldId;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface IMapper<E> {
     void add(E e);
-    void add(List<E> entities);
+    void add(Collection<E> entities);
     void update(E e);
     Integer findTotalNum(QueryRequest queryRequest);
     List<E> findAll(QueryRequest queryRequest);
